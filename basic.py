bfb679b60b1e1d22e53903b975ae6b978088fe17
ziptofaf1@gmail.com <ziptofaf1@gmail.com>
2017-06-15 13:59:46 +0200

    here, a simple trevis-based repo

---
+++ basic.py
@@ -0,0 +1,24 @@
+import csv
+
+def import_csv(filename):
+    file = open(filename)
+    reader = list(csv.reader(file))
+    return reader
+
+def get_row_data(row, *dataset):
+    data = dataset[0]
+    return data[0][row][0].split(';')
+
+def get_apple_data(*dataset):
+    return get_row_data(0, dataset)
+
+def get_income_on_row(row):
+    count = int(row[3])
+    price = float(row[2])
+    return count*price
+
+def build_shop_ui(*dataset):
+    return "" #todo add product list
+
+data = import_csv("shop.csv")
+print (get_apple_data(data))
---
+++ test_basic.py
@@ -0,0 +1,19 @@
+import basic
+
+def loading_test():
+    assert type(import_csv("shop.csv")).__name__ == "list"
+
+def test_apples_row():
+    data = import_csv("shop.csv")
+    apples = get_apple_data(data)
+    assert apples[1] == "apples"
+
+def test_income_on_apples():
+    data = import_csv("shop.csv")
+    apples = get_apple_data(data)
+    assert get_income_on_row(apples) == 70
+
+def test_ui():
+    data = import_csv("shop.csv")
+    res = build_shop_ui(data)
+    assert res == "apple:20,banana:5,orange:11,mackerel:2,vodka:99"
